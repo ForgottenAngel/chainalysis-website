652cd4de133a4a6e9fa4e0a13749fea822cb667e
ForgottenAngel <angelkanadetachibana150@gmail.com>
2021-10-22 05:45:21 -0400

    Updated PriceTable to be Dynamic
    Removed the static placeholder data to instead rely on the object's state and properties for values to display. In addition, added in PriceRetriever to fetch market data and have it updated every 2 seconds.

--- src/components/PriceTable.js
+++ src/components/PriceTable.js
@@ -1,23 +1,69 @@
 import React from "react";
+var priceFetch = require('./PriceRetriever.js');
 
 class PriceTable extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            BTC_buy: 0.0,
+            BTC_sell: 0.0,
+            ETH_buy: 0.0,
+            ETH_sell: 0.0
+        }
+    }
+
+    componentDidMount() {
+        this.tick();
+
+        this.timerID = setInterval(
+            () => this.tick(),
+            2000
+        );
+    }
+
+    componentWillUnmount() {
+        clearInterval(this.timerID);
+    }
+
+    async tick() {
+
+        let BTCBuyPrice = await priceFetch.retrievePrice("BTC", this.props.exchange, 'Buy');
+        let BTCSellPrice = await priceFetch.retrievePrice("BTC", this.props.exchange, 'Sell');
+        let ETHBuyPrice = await priceFetch.retrievePrice("ETH", this.props.exchange, 'Buy');
+        let ETHSellPrice = await priceFetch.retrievePrice("ETH", this.props.exchange, 'Sell');
+
+        this.setState({
+            BTC_buy: BTCBuyPrice,
+            BTC_sell: BTCSellPrice,
+            ETH_buy: ETHBuyPrice,
+            ETH_sell: ETHSellPrice 
+        });
+    }
+
     render() {
         return (
             <>
-                <table className='price-table'>
+                <table id={this.props.id} className='price-table'>
                     <thead>
                         <tr>
-                            <th colspan='2' className='table-website-header'>Website Name Here</th>
+                            <th colSpan='3' className='table-website-header'>{this.props.exchange}</th>
+                        </tr>
+                        <tr>
+                            <td> </td>
+                            <td>Buy Price</td>
+                            <td>Sell Price</td>
                         </tr>
                     </thead>
                     <tbody className='price-section'>
                         <tr>
                             <td>Bitcoin (BTC)</td>
-                            <td>$35,600.23</td>
+                            <td className='btc-buy-val'>${this.state.BTC_buy}</td>
+                            <td className='btc-sell-val'>${this.state.BTC_sell}</td>
                         </tr>
                         <tr>
                             <td>Etherium (ETH)</td>
-                            <td>$378.90</td>
+                            <td className='eth-buy-val'>${this.state.ETH_buy}</td>
+                            <td className='eth-sell-val'>${this.state.ETH_sell}</td>
                         </tr>
                     </tbody>
                 </table>
